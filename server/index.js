883e48cb401f52698bf2483a5702d7c9dba88cd0
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-07 15:56:03 +0300

    BugFix

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ mongoose
   })
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.DATABASE}`);
+      console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
   .catch((err) => console.log(err.message));
